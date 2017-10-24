6d6148006eb59a0e74142ec029b4f187d7740d17
Erin <erin@code.org>
2017-10-24 11:54:05 -0700

    WIP - Do Not Merge - Integrating QuickActions

--- apps/src/templates/teacherDashboard/SectionActionDropdown.jsx
+++ apps/src/templates/teacherDashboard/SectionActionDropdown.jsx
@@ -1,34 +1,9 @@
 import React, {Component, PropTypes} from 'react';
-import color from "../../util/color";
 import {sortableSectionShape} from "./shapes.jsx";
-import {OverlayTrigger} from 'react-bootstrap';
-import SectionActionBox from "./SectionActionBox";
-
-const styles = {
-  actionButton:{
-    border: '1px solid ' + color.white,
-    paddingLeft: 5,
-    paddingRight: 5,
-    paddingTop: 4,
-    paddingBottom: 4,
-    borderRadius: 5,
-    color: color.lighter_gray,
-    margin: 3
-  },
-  hoverFocus: {
-    backgroundColor: color.lighter_gray,
-    border: '1px solid ' + color.light_gray,
-    borderRadius: 5,
-    paddingTop: 4,
-    paddingBottom: 4,
-    paddingLeft: 5,
-    paddingRight: 5,
-    color: color.white,
-  },
-  actionBox: {
-    position: 'absolute',
-  },
-};
+import QuickAction from "../tables/QuickAction";
+import QuickActionsCell from "../tables/QuickActionsCell";
+import PrintCertificates from "./PrintCertificates";
+import i18n from '@cdo/locale';
 
 export default class SectionActionDropdown extends Component {
   static propTypes = {
@@ -38,29 +13,28 @@ export default class SectionActionDropdown extends Component {
 
   state = {
     selected: false,
+    names: [],
+  };
+
+  onClickEdit = () => {
+    this.props.handleEdit(this.props.sectionData.id);
   };
 
   render() {
+    const {sectionData} = this.props;
     return (
-      <OverlayTrigger
-        trigger="click"
-        rootClose
-        placement="bottom"
-        onEnter={() => this.setState({selected : true})}
-        onExit={() => this.setState({selected : false})}
-        overlay={
-          <SectionActionBox
-            sectionData={this.props.sectionData}
-            handleEdit={this.props.handleEdit}
-            style={styles.actionBox}
-            ref={actionBox => this.actionBox = actionBox}
-          />
-        }
-      >
-        <a style={this.state.selected ? {...styles.actionButton, ...styles.hoverFocus} : styles.actionButton}>
-          <i className="fa fa-chevron-down"></i>
-        </a>
-      </OverlayTrigger>
+      <QuickActionsCell>
+        <QuickAction
+          text={i18n.editSectionDetails()}
+          action={this.onClickEdit}
+          hasLineAbove={true}
+        />
+        <PrintCertificates
+          sectionId={sectionData.id}
+          assignmentName={sectionData.assignmentNames[0]}
+          onClick={() => console.log("Bleepty Bloop")}
+        />
+      </QuickActionsCell>
     );
   }
 }
